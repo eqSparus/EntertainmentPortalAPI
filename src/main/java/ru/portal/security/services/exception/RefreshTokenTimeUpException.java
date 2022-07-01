c73576ced871a815057be126ee68379256707171
eqSparus <sparus-1693@yandex.ru>
2022-07-01 16:24:57 +0600

    Ошибка истечения жизни токена обновления

--- src/main/java/ru/portal/security/services/exception/RefreshTokenTimeUpException.java
+++ src/main/java/ru/portal/security/services/exception/RefreshTokenTimeUpException.java
@@ -1,2 +1,32 @@
-package ru.portal.security.services.exception;public class RefreshTokenTimeUpException {
+package ru.portal.security.services.exception;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * Исключение бросаеться если время жизни токена обновления просрочено.<br>
+ * Сообщение по умолчанию {@value MESSAGE}
+ *
+ * @author Федорышин К.В.
+ */
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class RefreshTokenTimeUpException extends RuntimeException {
+
+    static String MESSAGE = "Время действия токена обновления истекло";
+
+    public RefreshTokenTimeUpException() {
+        super(MESSAGE);
+    }
+
+    public RefreshTokenTimeUpException(String message) {
+        super(message);
+    }
+
+    public RefreshTokenTimeUpException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public RefreshTokenTimeUpException(Throwable cause) {
+        super(MESSAGE, cause);
+    }
 }
