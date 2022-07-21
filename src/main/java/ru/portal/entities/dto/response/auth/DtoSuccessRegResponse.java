607f5574e838f98373e4f22cbf918f5311013679
eqSparus <sparus-1693@yandex.ru>
2022-07-21 23:00:04 +0600

    Изменения в dto

--- src/main/java/ru/portal/entities/dto/response/auth/DtoSuccessRegResponse.java
+++ src/main/java/ru/portal/entities/dto/response/auth/DtoSuccessRegResponse.java
@@ -1,15 +1,15 @@
 package ru.portal.entities.dto.response.auth;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Value;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.time.OffsetDateTime;
 
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-@Value
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class DtoSuccessRegResponse {
 
